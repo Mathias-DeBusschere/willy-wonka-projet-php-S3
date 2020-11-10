a7c481aca9093aca45f38de0a0cd481aa87bec82
dylan-pereira <dylan.pereira30@orange.fr>
2020-11-10 11:46:46 +0100

    gros premier refactor

---
+++ eCommerce/eCommerceTest/controller/routeur.php
@@ -0,0 +1,45 @@
+<?php
+//require_once File::build_path(array("controller","ControllerVoiture.php"));
+//require_once File::build_path(array("controller","ControllerUtilisateur.php"));
+//require_once File::build_path(array("controller","ControllerTrajet.php"));
+
+// On recupère l'action passée dans l'URL
+// if(isset($_GET['action'])){
+// 	$action = $_GET['action'];
+// }else{
+// 	$action = "readAll";
+// }
+
+// if(isset($_GET['controller'])){
+// 	$controller = $_GET['controller'];
+// }else{
+// 	$controller = "voiture";
+// }
+
+// $controller_class = 'Controller'.ucfirst($controller);
+// $class_methods = get_class_methods('ControllerVoiture');
+
+// if(class_exists($controller_class)){
+// 	if(in_array($action, $class_methods)){
+// 		$controller_class::$action(); 
+// 	}else{
+// 		$controller='voiture';
+// 	    $view='error';
+// 	    $pagetitle='Error';
+// 	    require File::build_path(array("view","view.php"));
+// 	}
+// }else{
+// 	$controller='voiture';
+//     $view='error';
+//     $pagetitle='Error';
+//     require File::build_path(array("view","view.php"));
+// }
+$view='conditions';
+$pagetitle='Accueil';
+require_once File::build_path(array("view","view.php"));
+
+// Appel de la méthode statique $action de ControllerVoiture
+
+
+
+?>
---
+++ eCommerce/eCommerceTest/view/cart.php
@@ -0,0 +1,13 @@
+<?php
+echo "
+	<h1 id=\"top_page_title\">Panier</h1>
+	<section class=\"cart\">
+		<h2>Votre panier est vide pour le moment.</h2>
+		<p>
+			Votre panier est là pour vous servir. Donnez-lui un but: remplissez le de chocolats.
+		</p>
+		<p>
+			Continuez vos achats sur <a href=\"chocolategeneral.html\">willywonka.fr</a>.
+		</p>
+	</section>"
+?>
